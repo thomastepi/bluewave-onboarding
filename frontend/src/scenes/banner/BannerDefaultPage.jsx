dd30967e71d8188f30108ca3a739558d68cd0004
Debora Serra <debora.r.serra@gmail.com>
2024-12-26 10:27:50 -0800

    fix: add isOpen condition to popup rendering

--- frontend/src/scenes/banner/BannerDefaultPage.jsx
+++ frontend/src/scenes/banner/BannerDefaultPage.jsx
@@ -1,39 +1,44 @@
-import React, { useState } from 'react';
-import DefaultPageTemplate from '../../templates/DefaultPageTemplate/DefaultPageTemplate';
-import { getBanners, deleteBanner } from '../../services/bannerServices';
-import { ACTIVITY_TYPES_INFO } from '../../data/guideMainPageData';
-import BannerPage from './CreateBannerPage';
+import React, { useState } from "react";
+import { ACTIVITY_TYPES_INFO } from "../../data/guideMainPageData";
+import { deleteBanner, getBanners } from "../../services/bannerServices";
+import DefaultPageTemplate from "../../templates/DefaultPageTemplate/DefaultPageTemplate";
+import { useDialog } from "../../templates/GuideTemplate/GuideTemplateContext";
+import BannerPage from "./CreateBannerPage";
 
 const BannerDefaultPage = () => {
-    const [itemsUpdated, setItemsUpdated] = useState(false);
-    const [isEdit, setIsEdit] = useState(false);
-    const [itemId, setItemId] = useState(null);
+  const [itemsUpdated, setItemsUpdated] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [itemId, setItemId] = useState(null);
 
-    const getBannerDetails = (banner) => ({
-        title: `Banner ${banner.id}`,
-        text: banner.bannerText,
-    });
+  const { isOpen } = useDialog();
 
-    return (
-        <>
-            <DefaultPageTemplate
-                getItems={getBanners}
-                deleteItem={deleteBanner}
-                setIsEdit={setIsEdit}
-                setItemId={setItemId}
-                itemType={ACTIVITY_TYPES_INFO.BANNERS}
-                itemTypeInfo={ACTIVITY_TYPES_INFO.BANNERS}
-                getItemDetails={getBannerDetails}
-                itemsUpdated={itemsUpdated}
-            />
-            <BannerPage
-                isEdit={isEdit}
-                itemId={itemId}
-                setItemsUpdated={setItemsUpdated}
-                setIsEdit={setIsEdit}
-            />
-        </>
-    );
+  const getBannerDetails = (banner) => ({
+    title: `Banner ${banner.id}`,
+    text: banner.bannerText,
+  });
+
+  return (
+    <>
+      <DefaultPageTemplate
+        getItems={getBanners}
+        deleteItem={deleteBanner}
+        setIsEdit={setIsEdit}
+        setItemId={setItemId}
+        itemType={ACTIVITY_TYPES_INFO.BANNERS}
+        itemTypeInfo={ACTIVITY_TYPES_INFO.BANNERS}
+        getItemDetails={getBannerDetails}
+        itemsUpdated={itemsUpdated}
+      />
+      {isOpen && (
+        <BannerPage
+          isEdit={isEdit}
+          itemId={itemId}
+          setItemsUpdated={setItemsUpdated}
+          setIsEdit={setIsEdit}
+        />
+      )}
+    </>
+  );
 };
 
 export default BannerDefaultPage;
--- frontend/src/scenes/popup/PopupDefaultPage.jsx
+++ frontend/src/scenes/popup/PopupDefaultPage.jsx
@@ -1,40 +1,44 @@
-import React, { useState } from 'react';
-import DefaultPageTemplate from '../../templates/DefaultPageTemplate/DefaultPageTemplate';
-import CreatePopupPage from './CreatePopupPage';
-import { getPopups, deletePopup } from '../../services/popupServices';
-import { ACTIVITY_TYPES_INFO } from '../../data/guideMainPageData';
+import React, { useState } from "react";
+import { ACTIVITY_TYPES_INFO } from "../../data/guideMainPageData";
+import { deletePopup, getPopups } from "../../services/popupServices";
+import DefaultPageTemplate from "../../templates/DefaultPageTemplate/DefaultPageTemplate";
+import { useDialog } from "../../templates/GuideTemplate/GuideTemplateContext";
+import CreatePopupPage from "./CreatePopupPage";
 
 const PopupDefaultPage = () => {
-    const [itemsUpdated, setItemsUpdated] = useState(false);
-    const [isEdit, setIsEdit] = useState(false);
-    const [itemId, setItemId] = useState(null);
+  const [itemsUpdated, setItemsUpdated] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+  const [itemId, setItemId] = useState(null);
 
-    const getPopupDetails = (popup) => ({
-        title: `Popup ${popup.id}`,
-        text: popup.header,
-    });
+  const { isOpen } = useDialog();
 
+  const getPopupDetails = (popup) => ({
+    title: `Popup ${popup.id}`,
+    text: popup.header,
+  });
 
-    return (
-      <>
-        <DefaultPageTemplate
-          getItems={getPopups}
-          deleteItem={deletePopup}
-          setIsEdit={setIsEdit}
-          setItemId={setItemId}
-          itemType={ACTIVITY_TYPES_INFO.POPUPS}
-          itemTypeInfo={ACTIVITY_TYPES_INFO.POPUPS}
-          getItemDetails={getPopupDetails}
-          itemsUpdated={itemsUpdated}
-        />
+  return (
+    <>
+      <DefaultPageTemplate
+        getItems={getPopups}
+        deleteItem={deletePopup}
+        setIsEdit={setIsEdit}
+        setItemId={setItemId}
+        itemType={ACTIVITY_TYPES_INFO.POPUPS}
+        itemTypeInfo={ACTIVITY_TYPES_INFO.POPUPS}
+        getItemDetails={getPopupDetails}
+        itemsUpdated={itemsUpdated}
+      />
+      {isOpen && (
         <CreatePopupPage
           isEdit={isEdit}
           itemId={itemId}
           setItemsUpdated={setItemsUpdated}
           setIsEdit={setIsEdit}
         />
-      </>
-    );
+      )}
+    </>
+  );
 };
 
 export default PopupDefaultPage;

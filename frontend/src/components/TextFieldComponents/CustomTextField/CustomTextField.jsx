7d55839f9a6c9a187a997e9301420404b75936db
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2025-01-02 09:21:02 -0500

    Bug fix

--- frontend/src/components/TextFieldComponents/CustomTextField/CustomTextField.jsx
+++ frontend/src/components/TextFieldComponents/CustomTextField/CustomTextField.jsx
@@ -38,7 +38,7 @@ const CustomTextField = ({
   const computedFullWidth = fullWidth ||
     ["full", "100%", "stretch"].some(value => TextFieldWidth.toLowerCase().includes(value));
   return (
-    <div className={...className} style={{ ...style, ...(computedFullWidth && { width: '100%' })}}>
+    <div className={className} style={{ ...style, ...(computedFullWidth && { width: '100%' })}}>
       {!checkCircleIconVisible &&
         <div>
           <InputLabel sx={{ fontWeight: labelFontWeight, margin: 0 }}>{labelText}</InputLabel>
@@ -101,6 +101,7 @@ const CustomTextField = ({
 };
 
 CustomTextField.propTypes = {
+  className: PropTypes.string,
   labelText: PropTypes.string,
   value: PropTypes.string,
   onChange: PropTypes.func,

76080a3b4b685307b4dbb24da3140c10a6ef2b96
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2025-01-02 04:52:44 -0500

    fixed mui design popup

--- frontend/src/templates/GuideMainPageTemplate/GuideMainPageComponents/ConfirmationPopup/ConfirmationPopup.jsx
+++ frontend/src/templates/GuideMainPageTemplate/GuideMainPageComponents/ConfirmationPopup/ConfirmationPopup.jsx
@@ -1,28 +1,42 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Button from "../../../../components/Button/Button";
+import "./ConfirmationPopupStyles.css";
 import {
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
   DialogTitle,
-  Button,
 } from "@mui/material";
 
 const ConfirmationPopup = ({ open, onConfirm, onCancel }) => {
   return (
-    <Dialog open={open} onClose={onCancel} closeAfterTransition={open}>
-      <DialogTitle>Confirm Action</DialogTitle>
-      <DialogContent>
+    <Dialog
+      PaperProps={{ className: "dialog-paper" }}
+      open={open}
+      onClose={onCancel}
+      closeAfterTransition={open}
+    >
+      <DialogTitle className="dialog-title">Confirm Action</DialogTitle>
+      <DialogContent className="dialog-content">
         <DialogContentText>
           Are you sure you want to perform this action?
         </DialogContentText>
       </DialogContent>
-      <DialogActions>
-        <Button onClick={onCancel}>Cancel</Button>
-        <Button onClick={onConfirm} color="primary">
-          Confirm
-        </Button>
+      <DialogActions className="dialog-actions">
+        <Button
+          text="Cancel"
+          buttonType="secondary"
+          variant="text"
+          onClick={onCancel}
+        />
+        <Button
+          text="Confirm"
+          onClick={onConfirm}
+          variant="contained"
+          buttonType="secondary"
+        />
       </DialogActions>
     </Dialog>
   );

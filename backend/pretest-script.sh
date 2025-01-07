839c41987c38925ab62aa722d48844a65c6ea991
Debora Serra <debora.r.serra@gmail.com>
2025-01-07 10:26:41 -0800

    docs: update pretest script

--- backend/pretest-script.sh
+++ backend/pretest-script.sh
@@ -3,15 +3,14 @@
 # set node env to test, if windows use set instead of export
 if [[ "$OSTYPE" == "msys" ]]; then
   if [ -n "$PSModulePath" ]; then
-    # PowerShell
     echo '$env:NODE_ENV="test"' | powershell -Command -
   else
-    # CMD
     cmd.exe /C "set NODE_ENV=test"
   fi
 else
   export NODE_ENV=test
 fi
+echo "NODE_ENV=$NODE_ENV"
 
 # remove test-postgres container if it exists
 if [[ "$(docker ps -a -q -f name=test-postgres)" ]]; then
@@ -19,26 +18,29 @@ if [[ "$(docker ps -a -q -f name=test-postgres)" ]]; then
 fi
 
 # run test-postgres container
-docker run --name test-postgres --env-file .env.test -p 5432:5432 -d postgres
+
+docker run --name test-postgres --env-file ./.env.test -p 5432:5432 -d postgres
 
 # wait for postgres to start
-npx wait-on tcp:5432
+echo "Waiting for PostgreSQL to be ready..."
+for i in {1..10}; do
+  if docker exec test-postgres pg_isready -U user123 >/dev/null 2>&1; then
+    echo "PostgreSQL is ready."
+    break
+  else
+    echo "PostgreSQL is not ready. Retrying ($i/10)..."
+    sleep 2
+  fi
+done
+echo "PostgreSQL is ready."
 
 # create test database if it doesn't exist
-if ! command -v psql &> /dev/null; then
-  # Check if database exists inside Docker container
-  if ! docker exec -it test-postgres psql -U postgres -tAc "SELECT 1 FROM pg_database WHERE datname='test_db'" | grep -q 1; then
-    npx sequelize-cli db:create --env test
-  else
-    echo "Database 'test_db' already exists in Docker."
-  fi
+exists=$(docker exec test-postgres psql -U user123 -d postgres -tAc "SELECT 1 FROM pg_database WHERE datname='onboarding_db_test'")
+echo "exists=$exists"
+if [[ -z "$exists" || "$exists" != *"1"* ]]; then
+  npx sequelize-cli db:create --env test
 else
-  # Check if database exists locally
-  if ! psql -U postgres -lqt | cut -d \| -f 1 | grep -qw test_db; then
-    npx sequelize-cli db:create --env test
-  else
-    echo "Database 'test_db' already exists locally."
-  fi
+  echo "Database 'onboarding_db_test' already exists in Docker."
 fi
 
 # run migrations

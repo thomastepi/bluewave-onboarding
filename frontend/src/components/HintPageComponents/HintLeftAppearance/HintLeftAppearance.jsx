0bfe2fb46591d91714a8d181e39abcaa48490165
Kannav02 <kannavsethi02@gmail.com>
2025-01-10 19:17:56 -0500

    (chore) imports and schema added

--- frontend/src/components/HintPageComponents/HintLeftAppearance/HintLeftAppearance.jsx
+++ frontend/src/components/HintPageComponents/HintLeftAppearance/HintLeftAppearance.jsx
@@ -1,13 +1,18 @@
 import React, { useMemo } from "react";
 import ColorTextField from "../../ColorTextField/ColorTextField";
 import "./HintLeftAppearance.css";
+  import { Formik, useFormikContext, Form } from "formik";
+  import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  
+});
 
 const HintLeftAppearance = ({ data = [] }) => {
   const content = useMemo(() => {
     if (data.length === 0) {
       return <div className="hint-appearance-container">No data available</div>;
     }
-
     return (
       <div className="hint-appearance-container">
         {data.map(({ stateName, state, setState }) => (

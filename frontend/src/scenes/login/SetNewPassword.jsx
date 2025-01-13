e17a4bfcdbc9f744caa16cbdd2924b74a3734d01
erenfn <erenfindik7@gmail.com>
2025-01-13 14:53:45 +0300

    some fixes to email and set new password

--- frontend/src/scenes/login/SetNewPassword.jsx
+++ frontend/src/scenes/login/SetNewPassword.jsx
@@ -50,7 +50,6 @@ function SetNewPasswordPage({ email = "asdf@asdf.com" }) {
           navigate("/reset-password");
         } catch (error) {
           console.error("Password Reset failed:", error);
-          console.log("error", error);
           if (error.response?.data?.errors) {
             setServerErrors(error.response?.data?.errors);
           } else if (error.response?.data?.error) {
@@ -71,112 +70,110 @@ function SetNewPasswordPage({ email = "asdf@asdf.com" }) {
         handleBlur,
         values,
       }) => (
-        <div className={styles["login-body"]}>
-          <Form className={styles["login-container"]}>
-            <h2 style={{ marginBottom: "0px" }}>Set new Password</h2>
-            <h3>
-              Your new password must be different from previously used passwords.
-            </h3>
-            <div className={styles["form-group"]}>
-              <CustomTextField
-                id="password"
-                type="password"
-                name="password"
-                labelText="Password*:"
-                checkCircleIconVisible={true}
-                displayCheckCircleIcon={touched.password && !errors.password}
-                placeholder="Create your password"
-                textFieldMargin="none"
-                TextFieldWidth="full"
-                required={true}
-                value={values.password}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={Boolean(touched.password && errors.password)}
-                helperText={touched.password && errors.password}
-              />
-            </div>
-            <div className={styles["form-group"]} style={{ marginBottom: 0 }}>
-              <CustomTextField
-                id="confirmPassword"
-                type="password"
-                name="confirmPassword"
-                labelText="Confirm Password*:"
-                checkCircleIconVisible={true}
-                displayCheckCircleIcon={
-                  touched.confirmPassword &&
-                  !errors.confirmPassword &&
-                  values.password === values.confirmPassword
-                }
-                placeholder="Confirm your password"
-                textFieldMargin="none"
-                TextFieldWidth="full"
-                required={true}
-                value={values.confirmPassword}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={Boolean(
-                  touched.confirmPassword && errors.confirmPassword
-                )}
-                helperText={touched.confirmPassword && errors.confirmPassword}
-              />
-              {serverErrors.length > 0 && (
-                <div className={styles["error-message"]}>
-                  {serverErrors.map((error, index) => (
-                    <div key={index}>{error}</div>
-                  ))}
-                </div>
+        <Form className={styles["login-container"]}>
+          <h2 style={{ marginBottom: "0px" }}>Set new Password</h2>
+          <h3>
+            Your new password must be different from previously used passwords.
+          </h3>
+          <div className={styles["form-group"]}>
+            <CustomTextField
+              id="password"
+              type="password"
+              name="password"
+              labelText="Password*:"
+              checkCircleIconVisible={true}
+              displayCheckCircleIcon={touched.password && !errors.password}
+              placeholder="Create your password"
+              textFieldMargin="none"
+              TextFieldWidth="full"
+              required={true}
+              value={values.password}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={Boolean(touched.password && errors.password)}
+              helperText={touched.password && errors.password}
+            />
+          </div>
+          <div className={styles["form-group"]} style={{ marginBottom: '10px' }}>
+            <CustomTextField
+              id="confirmPassword"
+              type="password"
+              name="confirmPassword"
+              labelText="Confirm Password*:"
+              checkCircleIconVisible={true}
+              displayCheckCircleIcon={
+                touched.confirmPassword &&
+                !errors.confirmPassword &&
+                values.password === values.confirmPassword
+              }
+              placeholder="Confirm your password"
+              textFieldMargin="none"
+              TextFieldWidth="full"
+              required={true}
+              value={values.confirmPassword}
+              onChange={handleChange}
+              onBlur={handleBlur}
+              error={Boolean(
+                touched.confirmPassword && errors.confirmPassword
               )}
-            </div>
+              helperText={touched.confirmPassword && errors.confirmPassword}
+            />
+            {serverErrors.length > 0 && (
+              <div className={styles["error-message"]}>
+                {serverErrors.map((error, index) => (
+                  <div key={index}>{error}</div>
+                ))}
+              </div>
+            )}
+          </div>
 
-            <div className={styles["password-constraints"]}>
-              <CheckCircleIcon
-                style={{
-                  color:
-                    values.password.length >= 8
-                      ? "green"
-                      : "var(--light-border-color)",
-                  fontSize: "20px",
-                  marginRight: "5px",
-                }}
-              />
-              Must be at least 8 characters
-            </div>
-            <div className={styles["password-constraints"]}>
-              <CheckCircleIcon
-                style={{
-                  color: /[!@#$%^&*(),.?":{}|<>_\-=]/.test(values.password)
+          <div className={styles["password-constraints"]}>
+            <CheckCircleIcon
+              style={{
+                color:
+                  values.password.length >= 8
                     ? "green"
                     : "var(--light-border-color)",
-                  fontSize: "20px",
-                  marginRight: "5px",
-                }}
-              />
-              Must contain one special character
-            </div>
-            <button
-              type="submit"
-              className={styles["sign-in-button"]}
-              style={{ marginTop: "15px" }}
-              disabled={isSubmitting}
-            >
-              {isSubmitting ? (
-                <CircularProgress size={12} color="inherit" />
-              ) : (
-                "Reset Password"
-              )}
-            </button>
-            <button
-              type="button"
-              onClick={() => navigate("/login")}
-              className={styles["back-to-login-button"]}
-            >
-              {" "}
-              <ArrowBackIcon style={{ fontSize: "18px", marginRight: "5px" }} />
-              Back to log in
-            </button>
-          </Form>
-        </div>
+                fontSize: "20px",
+                marginRight: "5px",
+              }}
+            />
+            Must be at least 8 characters
+          </div>
+          <div className={styles["password-constraints"]}>
+            <CheckCircleIcon
+              style={{
+                color: /[!@#$%^&*(),.?":{}|<>_\-=]/.test(values.password)
+                  ? "green"
+                  : "var(--light-border-color)",
+                fontSize: "20px",
+                marginRight: "5px",
+              }}
+            />
+            Must contain one special character
+          </div>
+          <button
+            type="submit"
+            className={styles["sign-in-button"]}
+            style={{ marginTop: "15px" }}
+            disabled={isSubmitting}
+          >
+            {isSubmitting ? (
+              <CircularProgress size={12} color="inherit" />
+            ) : (
+              "Reset Password"
+            )}
+          </button>
+          <button
+            type="button"
+            onClick={() => navigate("/login")}
+            className={styles["back-to-login-button"]}
+          >
+            {" "}
+            <ArrowBackIcon style={{ fontSize: "18px", marginRight: "5px" }} />
+            Back to log in
+          </button>
+        </Form>
       )}
     </Formik>
   );

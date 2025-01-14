82e57001fe28e185482392c96b5673aacd707c7a
Kannav02 <kannavsethi02@gmail.com>
2025-01-13 22:56:27 -0500

    corrected the validationSchema for HintAppearence

--- frontend/src/components/HintPageComponents/HintLeftAppearance/HintLeftAppearance.jsx
+++ frontend/src/components/HintPageComponents/HintLeftAppearance/HintLeftAppearance.jsx
@@ -4,8 +4,27 @@ import "./HintLeftAppearance.css";
   import { Formik, useFormikContext, Form } from "formik";
   import * as Yup from "yup";
 
-const validationSchema = Yup.object().shape({
-  
+const colorRegex = /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/;
+const validationSchema = Yup.object({
+  headerBackgroundColor: Yup.string()
+  .required("Header background color is required")
+  .matches(colorRegex,"Header background must be a valid hex color"),
+
+  headerColor: Yup.string()
+  .required("Header color is required")
+  .matches(colorRegex,"Header color must be a valid hex color"),
+
+  textColor: Yup.string()
+  .required("Text color is required")
+  .matches(colorRegex,"Text color must be a valid hex color"),
+
+  buttonBackgroundColor: Yup.string()
+  .required("Button background color is required")
+  .matches(colorRegex,"Button background must be a valid hex color"),
+
+  buttonTextColor: Yup.string()
+  .required("Button text color is required")
+  .matches(colorRegex,"Button text color must be a valid hex color"),
 });
 
 const HintLeftAppearance = ({ data = [] }) => {

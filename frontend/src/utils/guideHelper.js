fba07c6b7c23d740c7f05d3cbda4d63d35359616
Debora Serra <debora.r.serra@gmail.com>
2025-01-28 18:15:13 -0800

    feat: add new fields to front end service functions

--- frontend/src/utils/guideHelper.js
+++ frontend/src/utils/guideHelper.js
@@ -3,7 +3,7 @@ import toastEmitter, { TOAST_EMITTER_KEY } from './toastEmitter';
 export const emitToastError = (error) => {
     if (error.response?.data) {
       for (let i = 0; i < error.response.data.errors.length; i++){
-        toastEmitter.emit(TOAST_EMITTER_KEY, 'An error occurred: ' + error.response.data.errors[i])
+        toastEmitter.emit(TOAST_EMITTER_KEY, 'An error occurred: ' + error.response.data.errors[i].msg)
       }
     } else {
         toastEmitter.emit(TOAST_EMITTER_KEY, 'An error occurred. Please check your network connection and try again.')

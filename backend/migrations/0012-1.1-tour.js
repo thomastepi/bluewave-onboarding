7bcdabfe553107bea0581bcc94ee0edf4edca867
Debora Serra <debora.r.serra@gmail.com>
2025-02-05 13:32:04 -0800

    feat: add migrations

---
+++ backend/migrations/0012-1.1-tour.js
@@ -0,0 +1,111 @@
+'use strict';
+
+const { validateHexColor } = require('../src/utils/guide.helper');
+
+const TABLE_NAME = 'tour';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  /**
+   *
+   * @param {import("sequelize").QueryInterface} queryInterface
+   * @param {import("sequelize").Sequelize} Sequelize
+   */
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.createTable(
+        TABLE_NAME,
+        {
+          id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER,
+          },
+          headerColor: {
+            type: Sequelize.STRING(15),
+            allowNull: false,
+            defaultValue: '#101828',
+            validate: {
+              isHexColor(value) {
+                validateHexColor(value, 'headerColor');
+              },
+            },
+          },
+          textColor: {
+            type: Sequelize.STRING(15),
+            allowNull: false,
+            defaultValue: '#344054',
+            validate: {
+              isHexColor(value) {
+                validateHexColor(value, 'textColor');
+              },
+            },
+          },
+          buttonBgColor: {
+            type: Sequelize.STRING(15),
+            allowNull: false,
+            defaultValue: '#7F56D9',
+            validate: {
+              isHexColor(value) {
+                validateHexColor(value, 'buttonBgColor');
+              },
+            },
+          },
+          buttonTextColor: {
+            type: Sequelize.STRING(15),
+            allowNull: false,
+            defaultValue: '#ffffff',
+            validate: {
+              isHexColor(value) {
+                validateHexColor(value, 'buttonTextColor');
+              },
+            },
+          },
+          size: {
+            type: Sequelize.ENUM('small', 'medium', 'large'),
+            allowNull: false,
+            defaultValue: 'medium',
+          },
+          finalBtnText: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            defaultValue: 'Complete tour',
+          },
+          createdBy: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'users',
+              key: 'id',
+            },
+          },
+        },
+        { transaction }
+      );
+
+      // Commit the transaction
+      await transaction.commit();
+    } catch (error) {
+      // Rollback the transaction in case of an error
+      await transaction.rollback();
+      throw error;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      // Drop the guide_logs table
+      await queryInterface.dropTable(TABLE_NAME, { transaction });
+
+      // Commit the transaction
+      await transaction.commit();
+    } catch (error) {
+      // Rollback the transaction in case of an error
+      await transaction.rollback();
+      throw error;
+    }
+  },
+};
---
+++ backend/migrations/0013-1.1-tour-popup.js
@@ -0,0 +1,73 @@
+'use strict';
+
+const TABLE_NAME = 'tour_popup';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.createTable(
+        TABLE_NAME,
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          title: {
+            type: Sequelize.STRING(255),
+            allowNull: false,
+          },
+          description: {
+            type: Sequelize.TEXT,
+            allowNull: false,
+          },
+          targetElement: {
+            type: Sequelize.STRING(255),
+            allowNull: false,
+          },
+          url: {
+            type: Sequelize.STRING(255),
+            allowNull: false,
+          },
+          order: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            defaultValue: 1,
+          },
+          target: {
+            type: Sequelize.BOOLEAN,
+            defaultValue: true,
+            allowNull: true,
+          },
+          tourId: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            references: {
+              model: 'tours',
+              key: 'id',
+            },
+          },
+        },
+        { transaction }
+      );
+
+      // Commit the transaction
+      await transaction.commit();
+    } catch (error) {
+      // Rollback the transaction in case of an error
+      await transaction.rollback();
+      throw error;
+    }
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  },
+};

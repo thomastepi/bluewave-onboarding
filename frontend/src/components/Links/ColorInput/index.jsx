1d585f49e3f16ada2361fb8fe89aac28a70fa5d6
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2025-02-13 07:45:09 -0500

    Updated colorInput field to support formik behaviour

--- frontend/src/components/Links/ColorInput/index.jsx
+++ frontend/src/components/Links/ColorInput/index.jsx
@@ -1,43 +1,68 @@
 import PropTypes from 'prop-types';
+import { useField } from 'formik';
 import styles from './ColorInput.module.scss';
 
-const ColorInput = ({ id, name, value, onChange, onBlur, error, title, className }) => (
-  <label htmlFor="header-bg" className={styles.appearance__label}>
-    {title}{' '}
-    <div className={styles.appearance__color}>
-      <span
-        className={`${styles.appearance__input} ${styles[className]} ${
-          error && styles.error
-        }`}
-      >
-        {value}
-      </span>
-      <div className={styles.appearance__circle}>
-        <input
-          type="color"
-          id={id}
-          name={name}
-          value={value}
-          onChange={onChange}
-          onBlur={onBlur}
-        />
-        <span
-          className={styles['appearance__circle--mask']}
-          style={{ backgroundColor: value }}
-        />
-      </div>
-    </div>
-  </label>
-);
+const ColorInput = ({ id, title, className, ...props }) => {
+  // To thread formik behaviour into custom field
+  const [field, meta, helpers] = useField(props.name);
+  const { error, touched } = meta;
+  const { setValue, setTouched } = helpers;
 
-export default ColorInput;
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <label htmlFor={id} className={styles.appearance__label}>
+      {title}
+      <div className={styles.appearance__color}>
+        <input
+          type="text"
+          {...field}
+          value={field.value}
+          onChange={handleChange}
+          className={`${styles.appearance__input} ${
+            className ? styles[className] : ''
+          } ${error && touched ? styles.error : ''}`}
+          onBlur={() => setTouched(true)}
+        />
+        <div className={styles.appearance__circle}>
+          <input
+            type="color"
+            id={id}
+            value={field.value}
+            onChange={handleChange}
+            style={{ zIndex: 10, cursor: 'pointer', opacity: 0 }}
+            onBlur={() => setTouched(true)}
+          />
+          {error && touched ? (
+            <img
+              src="/no-background.jpg"
+              alt="No color selected"
+              className={styles['appearance__circle--mask']}
+            />
+          ) : (
+            <span
+              className={styles['appearance__circle--mask']}
+              style={{ backgroundColor: field.value }}
+            />
+          )}
+        </div>
+      </div>
+      {error && touched && (
+        <small className={styles['appearance__error']} aria-live="polite">
+          {error}
+        </small>
+      )}
+    </label>
+  );
+};
 
 ColorInput.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  value: PropTypes.string.isRequired,
-  onChange: PropTypes.func.isRequired,
-  onBlur: PropTypes.func.isRequired,
-  error: PropTypes.string,
   title: PropTypes.string,
+  className: PropTypes.string,
 };
+
+export default ColorInput;

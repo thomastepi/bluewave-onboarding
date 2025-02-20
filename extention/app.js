712477f549ba8ef87904067b5f8fd65edc2d8299
Debora Serra <debora.r.serra@gmail.com>
2025-02-19 17:53:52 -0800

    feat: add dragging function and style

--- extention/app.js
+++ extention/app.js
@@ -3,10 +3,13 @@ const DIV_ID = "bw-sticky-botton-div";
 const MENU_ID = "bw-ext-floating-menu";
 const INPUT_ID = "bw-ext-input";
 const BUTTON_ID = "bw-ext-button";
+const TOUR_STEPS_CONTAINER_ID = "bw-ext-tour-steps";
+const TOUR_STEP_CLASS = "bw-ext-tour-step";
 const AVAILABLE_MODES = ["hint", "tour"];
 let domSelected = false;
 let lastHighlightTarget;
-let selectedMode = "hint";
+let selectedMode = "tour";
+let selectedElements = [];
 
 function terminate() {
   // The `click` listener is automatically removed after it has been called once
@@ -35,15 +38,8 @@ function createFloatingMenu() {
   div.style.backgroundColor = "#f8f9fa";
   div.style.padding = "10px";
   div.style.cursor = "pointer";
-  div.style.minWidth = "100px";
-  // div.addEventListener('mouseenter', () => {
-  // 	div.style.opacity = '1';
-  // 	div.style.transform = 'translateY(0)';
-  // });
-  // div.addEventListener('mouseleave', () => {
-  // 	div.style.opacity = '0';
-  // 	div.style.transform = 'translateY(100%)';
-  // });
+  div.style.minWidth = "160px";
+  div.style.alignItems = "center";
   document.body.appendChild(div);
 
   const buttonContainer = document.createElement("div");
@@ -52,6 +48,20 @@ function createFloatingMenu() {
   buttonContainer.style.borderRadius = "30px";
   buttonContainer.style.overflow = "hidden";
   buttonContainer.style.backgroundColor = "#ccc";
+
+  const cardContainer = document.createElement("div");
+  cardContainer.id = TOUR_STEPS_CONTAINER_ID;
+  cardContainer.style.display = "flex";
+  cardContainer.style.flexDirection = "column";
+  cardContainer.style.gap = "10px";
+  cardContainer.style.width = "100%";
+  cardContainer.style.maxHeight = "300px";
+  cardContainer.style.overflowY = "auto";
+
+  div.appendChild(buttonContainer);
+  div.appendChild(cardContainer);
+
+  addDragEvent();
 
   AVAILABLE_MODES.forEach((mode) => {
     const button = document.createElement("button");
@@ -73,7 +83,6 @@ function createFloatingMenu() {
     button.addEventListener("click", () => {
       selectedMode = mode;
       buttonContainer.childNodes.forEach((button) => {
-        console.log({ selectedMode, button: button.textContent });
         if (button.textContent !== selectedMode) {
           button.style.backgroundColor = "#ccc";
           button.style.color = "#333";
@@ -83,11 +92,19 @@ function createFloatingMenu() {
         }
       });
     });
+    if (mode === "tour") {
+      generateList();
+    } else {
+      cardContainer.innerHTML = "";
+    }
     buttonContainer.appendChild(button);
   });
 
   // add event listener to drag the menu around
   div.addEventListener("mousedown", (e) => {
+    if (e.target.id !== MENU_ID) {
+      return;
+    }
     const initialX = e.clientX - div.getBoundingClientRect().left;
     const initialY = e.clientY - div.getBoundingClientRect().top;
 
@@ -104,8 +121,68 @@ function createFloatingMenu() {
     document.addEventListener("mousemove", onMouseMove);
     document.addEventListener("mouseup", onMouseUp);
   });
+}
 
-  div.appendChild(buttonContainer);
+function addDragEvent() {
+  const list = document.getElementById(TOUR_STEPS_CONTAINER_ID);
+  let draggingItem = null;
+
+  list.addEventListener("dragstart", (e) => {
+    draggingItem = e.target;
+    e.target.classList.add("dragging");
+  });
+
+  list.addEventListener("dragend", (e) => {
+    e.target.classList.remove("dragging");
+    document
+      .querySelectorAll(`.${TOUR_STEP_CLASS}`)
+      .forEach((item) => item.classList.remove("over"));
+    draggingItem = null;
+    // update the step according to the new order
+    const newList = Array.from(list.children).map((item, index) => {
+      const step = item.id.replace("step-", "");
+      const element = selectedElements.find((el) => el.step === +step);
+      return { ...element, step: index + 1 };
+    });
+    selectedElements = newList;
+    generateList();
+  });
+
+  list.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const draggingOverItem = getDragAfterElement(list, e.clientY);
+
+    // Remove .over from all items
+    document
+      .querySelectorAll(`.${TOUR_STEP_CLASS}`)
+      .forEach((item) => item.classList.remove("over"));
+
+    if (draggingOverItem) {
+      draggingOverItem.classList.add("over"); // Add .over to the hovered item
+      list.insertBefore(draggingItem, draggingOverItem);
+    } else {
+      list.appendChild(draggingItem); // Append to the end if no item below
+    }
+  });
+
+  function getDragAfterElement(container, y) {
+    const draggableElements = [
+      ...container.querySelectorAll(`.${TOUR_STEP_CLASS}:not(.dragging)`),
+    ];
+
+    return draggableElements.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      { offset: Number.NEGATIVE_INFINITY }
+    ).element;
+  }
 }
 
 function createStickyDiv() {
@@ -202,7 +279,11 @@ function addHighlight() {
 }
 
 function updateHighlight({ target }) {
-  if (!(target instanceof HTMLElement) || target === lastHighlightTarget) {
+  if (
+    !(target instanceof HTMLElement) ||
+    target === lastHighlightTarget ||
+    target.id.startsWith("bw-")
+  ) {
     return;
   }
   lastHighlightTarget = target;
@@ -228,10 +309,86 @@ function removeHighlight() {
   domSelected = true;
 }
 
+function generateList() {
+  const trashClass = "fa";
+  const dragClass = "drag-icon";
+  const cardTemplate = `<i class="${dragClass}">&equiv;</i><span>Step {{step}}</span><i class="${trashClass}">&#xf014;</i></i>`;
+  const cardContainer = document.getElementById(TOUR_STEPS_CONTAINER_ID);
+  cardContainer.innerHTML = "";
+
+  selectedElements.forEach(({ element, step }, index) => {
+    const card = document.createElement("div");
+    card.classList.add("bw-ext-tour-step");
+    card.id = `step-${step}`;
+
+    // add card style
+    card.draggable = true;
+    card.innerHTML = cardTemplate.replace("{{step}}", step);
+    card.style.display = "flex";
+    card.style.alignItems = "center";
+    card.style.gap = "10px";
+    card.style.color = "#344054";
+    card.style.border = "1px solid #D0D5DD";
+    card.style.borderRadius = "8px";
+    card.style.padding = "8px";
+
+    const content = card.querySelector("span");
+    content.style.flexGrow = "1";
+
+    const dragIcon = card.querySelector(`.${dragClass}`);
+    dragIcon.style.cursor = "grab";
+    dragIcon.style.fontSize = "1.3rem";
+    dragIcon.style.color = "#98A2B3";
+
+    const trashIcon = card.querySelector(`.${trashClass.replace(/\s/g, ".")}`);
+    trashIcon.style.cursor = "pointer";
+    trashIcon.style.fontSize = "1.3rem";
+    trashIcon.style.color = "#667085";
+    trashIcon.addEventListener("click", () => {
+      selectedElements.splice(index, 1);
+      generateList();
+      const target = document.querySelector(element);
+      if (target) {
+        target.style.outline = "none";
+      }
+    });
+
+    cardContainer.appendChild(card);
+
+    card.addEventListener("mouseenter", () => {
+      card.style.borderColor = "#7F56D9";
+      addHighlight();
+      const target = document.querySelector(element);
+      if (target) {
+        target.style.outline = "2px solid #7F56D9";
+      }
+    });
+    card.addEventListener("mouseleave", () => {
+      card.style.borderColor = "#D0D5DD";
+      const target = document.querySelector(element);
+      if (target) {
+        target.style.outline = "none";
+      }
+    });
+  });
+}
+
 async function grabSelector(event) {
   event.preventDefault();
   const { target } = event;
-
+  const isBw = target.closest("[id^=bw-]");
+  if (isBw) {
+    return;
+  }
+  if (selectedMode === "tour") {
+    const selector = generateSelector(target);
+    selectedElements.push({
+      element: selector,
+      step: selectedElements.length + 1,
+    });
+    generateList();
+    return;
+  }
   if (!(target instanceof HTMLElement)) {
     terminate();
     return;

76d4d3e284e7f30eabdbfb621245a3d6604b20ec
Swapnil <swapnil118@icloud.com>
2025-03-29 21:08:42 -0400

    Skeleton component created

---
+++ frontend/src/scenes/banner/BannerPageComponents/BannerPreview/BannerSkeleton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './BannerPreview.module.scss';
+
+export default function BannerSkeleton() {
+  return (
+    <>
+      <div className={styles.bannerOne}></div>
+      <div className={styles.bannerTwo}></div>
+      <div className={styles.bannerThree}></div>
+      <div className={styles.bannerFour}></div>{' '}
+    </>
+  );
+}
